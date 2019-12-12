3ed2c1f1dde1cf9f07a056abef56ca137b96b5a7
Lucas Pavanelli <lucasapava@gmail.com>
2019-12-12 15:58:34 -0300

    Update routes

--- smart_house.py
+++ smart_house.py
@@ -27,42 +27,43 @@ collection = table["sensores"]
 @app.route("/insert-component", methods=['POST'])
 def insert_component():
     content = request.get_json()
-    collection.insert(content)
-    return "Adicionou um componente ao banco"
+    response = collection.insert(content)
+    result = {"id": str(response)}
+    return result
 
 
-@app.route("/control-motor/<string:comp_id>/<float:speed>/<int:mode>")
+@app.route("/control-motor/<string:comp_id>/<float:speed>/<int:mode>", methods=['GET'])
 def control_motor(comp_id, speed, mode):
     motor_data = find_component_db(comp_id)
     if motor_data is None:
-        return "Erro ao encontrar motor"
-    port_list = motor_data['porta']
+        return "Erro ao encontrar motor", 500
+    port_list = motor_data['ports']
     if len(port_list) < 2:
-        return "Motor deve ter duas portas especificadas"
+        return "Motor deve ter duas portas especificadas", 500
     motor = Motor(port_list[0], port_list[1])
     if mode > 0:
         motor.forward(speed)
     else:
         motor.backward(speed)
-    return "Sucesso"
+    return "Sucesso", 200
 
 
 @app.route("/control-led/<int:comp_id>/<int:mode>")
 def control_led(comp_id, mode):
     led_data = find_component_db(comp_id)
     if led_data is None:
-        return "Erro ao encontrar led"
-    port = led_data['porta'][0]
+        return "Erro ao encontrar led", 500
+    port = led_data['ports'][0]
     led = LED(port)
     if mode > 0:
         led.on()
     else:
         led.off()
-    return "Sucesso!"
+    return "Sucesso!", 200
 
 
 def find_component_db(comp_id):
-    data = {"id": comp_id}
+    data = {"_id": comp_id}
     return collection.find_one(data)
 
 # rode o servidor
